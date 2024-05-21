701b5add5af8451efb2a355f51288c302753b378
Luiz Vinicius Cunha Maciel <viniciuspalmeirense75@gmail.com>
2024-05-21 14:54:05 -0300

    Login Screen

---
+++ src/Main/YttriumSuperMarket.java
@@ -0,0 +1,13 @@
+
+package Main;
+
+import View.LoginScreen;
+
+public class YttriumSuperMarket {
+
+    public static void main(String[] args) {
+       LoginScreen ls = new LoginScreen();
+       ls.setVisible(true);
+    }
+    
+}
